dfa6728a3596f704d541829a9b8496769fee7e6c
asap <asap@dfbce8c5-1874-46a4-954d-4d2953228b1d>
2011-08-18 04:51:00 +0000

    fixed mime type file download code
    git-svn-id: svn+ssh://chitna.asap.um.maine.edu/var/svn/grad_systeminterface2/trunk@7 dfbce8c5-1874-46a4-954d-4d2953228b1d

--- getFile.php
+++ getFile.php
@@ -7,19 +7,18 @@ include_once 'lib/core.php';
 //make sure user is valid
 if(check_ses_vars() != '') {
 
+	// Build File
 	if ($_GET['FileType'] == 'application') {
-		$file = $GLOBALS['completed_pdf_directory'] . $_GET['FileName'];
-		header("Content-Type: application/pdf");
-	
+		$file = $GLOBALS['completed_pdf_directory'] . $_GET['FileName'];	
 	} else if($_GET['FileType'] == 'essay') {
 		$file = $GLOBALS['completed_essay_directory'] . $_GET['FileName'];
-		header("Content-Type: application/pdf");
-	
 	} else if($_GET['FileType'] == 'resume') {
 		$file = $GLOBALS['completed_resume_directory'] . $_GET['FileName'];
-		header("Content-Type: application/pdf");
 	}
-
+	
+	// Build Content-Type
+	header("Content-Type: " . mime_content_type($_GET['FileName']) );
+	
 	header("Content-Length: " . filesize($file));
 	readfile($file);
 
