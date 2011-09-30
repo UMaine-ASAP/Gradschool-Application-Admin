bf6b3a8b5264a1e5db8898eb2b703beba5d9e80a
asap <asap@dfbce8c5-1874-46a4-954d-4d2953228b1d>
2011-09-30 14:42:46 +0000

    git-svn-id: svn+ssh://chitna.asap.um.maine.edu/var/svn/grad_systeminterface2/trunk@54 dfbce8c5-1874-46a4-954d-4d2953228b1d

--- scripts/login.php
+++ scripts/login.php
@@ -9,7 +9,7 @@ if( isset($_POST['username']) && isset($_POST['password']) ) {
 	$db = Database::get();
 	$r = $db->query("SELECT id, password FROM admin WHERE username = %s", $_POST['username']);
 	$result = $r[0];
-	if( $result['password'] == sha1($_POST['password']) ) {
+	if( $result != null && $result['password'] == sha1($_POST['password']) ) {
 		user_login($result['id']);
 		echo 'success';
 	} else { 
